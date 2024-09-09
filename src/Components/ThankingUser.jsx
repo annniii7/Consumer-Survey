4dd8f25ecaeda9f14a97e274bcc6f563eadc1546
Anirudh <anirudhsharma4921@gmail.com>
2024-09-09 23:15:09 +0530

    minute change

--- src/Components/ThankingUser.jsx
+++ src/Components/ThankingUser.jsx
@@ -5,12 +5,12 @@ const ThankingUser = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Set a timeout to redirect to the homepage after 5 seconds (5000 milliseconds)
+  
     const timer = setTimeout(() => {
-      navigate('/'); // Redirect to homepage ("/" can be the route of your homepage)
+      navigate('/'); 
     }, 5000);
 
-    // Cleanup the timer if the component unmounts before the timeout is complete
+    
     return () => clearTimeout(timer);
   }, [navigate]);
 
